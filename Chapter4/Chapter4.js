26b2f2f221d10bd2f1df229a2386756afd8b25ef
ilonasamko <samkoilona@gmail.com>
2021-03-29 15:25:42 +0300

    Chapter4

---
+++ Chapter4/Chapter4.js
@@ -0,0 +1,26 @@
+//HW #1
+
+let scores = {
+    Amely: 0,
+    Maks: 0
+};
+
+scores.Amely += 5;
+scores.Maks += 25;
+console.log(scores);
+scores.Amely += 133;
+scores.Maks += 88;
+console.log(scores);
+
+
+
+//HW #2
+
+let myCrazyObject = {
+    "name": "Some object",
+    "some array": [7, 9, { purpose: "confuse", number: 123 }, 3.3],
+    "random animal": "Banana shark"
+}
+console.log(myCrazyObject["some array"][2].number);
+
+
